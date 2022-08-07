63a87af510af0e9630ecf13a62bd3976175080e8
Calvin Hubbard <caljay@wayjay.com>
2022-08-06 20:23:44 -0700

    custom input added, still utterly broken lol

--- Assembly-Calculator/main.cpp
+++ Assembly-Calculator/main.cpp
@@ -8,7 +8,22 @@ int main(void) {
 
 	// < 1 as 3rd = add 
 	// = 1 as 3rd = sub
-	std::cout << "Answer is: " << start(1, 1, 1);
+
+	std::cout << "Welcome to the Assembly Calculator!" << std::endl << "Please choose an operations 0 for addition 1 for subtration" << std::endl;
+	int inputOperations = 0;
+	std::cin >> inputOperations;
+
+
+	int num1, num2;
+	std::cout << std::endl << "Now Enter First Number" << std::endl;
+	std::cin >> num1; 
+	std::cout << std::endl << "Now Enter SecondNumber Number" << std::endl;
+	std::cin >> num2;
+
+
+	
+
+	std::cout << "Answer is: " << start(num1, num2, inputOperations);
 	std::cin.get();
 	return 0;
 }
